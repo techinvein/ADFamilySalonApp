b78f7dc3f05539c8a8645de94084037aed1f29ac
somnathivan <somnath.ivan@gmail.com>
2019-06-27 16:40:54 +0530

    first commit

---
+++ src/pages/profile/profile.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-profile',
+  templateUrl: 'profile.html',
+})
+export class ProfilePage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ProfilePage');
+  }
+
+/*  ion-item pencil icon function. 
+    Please change individual function name for every pencil icon 
+*/
+clickCM(){
+  alert("Edit icon clicked..")
+}
+
+uploadCam(){
+  alert("Functionality no implemented yet.")
+}
+
+
+}
