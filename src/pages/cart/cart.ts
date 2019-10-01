030fdf5402e6d986abc8eae90374e6644a3e1d4f
pc-145 <sarghyadeep@gmail.com>
2019-10-01 18:42:45 +0530

    cart page some work done

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ModalController } from 'ionic-angular/components/modal/modal-controller';
 import { ActionSheetController } from 'ionic-angular/components/action-sheet/action-sheet-controller';
+import * as firebase from 'firebase';
 
 /**
  * Generated class for the CartPage page.
@@ -17,11 +18,63 @@ import { ActionSheetController } from 'ionic-angular/components/action-sheet/act
 })
 export class CartPage {
   payMethod:any = "Cash after Service";
+  allCartProducts: any = [];
+  cartTotal: any;
+  currentUser: any
+
   constructor(public navCtrl: NavController,public actionSheetCtrl: ActionSheetController, public navParams: NavParams,public modalCtrl: ModalController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CartPage');
+    this.currentUser = firebase.auth().currentUser.uid;
+    if(this.currentUser) {
+      firebase.database().ref('users/' + this.currentUser + '/cart/').on('value',(snap)=>{
+        console.log(snap.val());
+        if(snap.val()) {
+          let allProducts = snap.val();
+          let cartProducts = []
+          for(let key in allProducts) {
+            allProducts[key].uid = key;
+            cartProducts.push(allProducts[key]);
+          }
+          this.allCartProducts = cartProducts;
+          console.log(this.allCartProducts)
+          this.cartTotalCalc()
+        }
+        else {
+          this.allCartProducts = []
+        }
+      })
+    }
+  }
+
+  cartTotalCalc() {
+    let cartTotal = 0;
+    for(let i=0; i< this.allCartProducts.length; i++) {
+      cartTotal = cartTotal + this.allCartProducts[i].subcat_price;
+    }
+    this.cartTotal = cartTotal;
+  }
+
+  increment(item) {
+    let price = item.original_price * (item.qty + 1)
+    firebase.database().ref('users/' + this.currentUser + '/cart/' + item.uid + '/').update({
+      qty: (item.qty + 1),
+      subcat_price: price
+    })
+  }
+
+  decrement(item) {
+    let price = item.original_price * (item.qty - 1)
+    firebase.database().ref('users/' + this.currentUser + '/cart/' + item.uid + '/').update({
+      qty: (item.qty - 1),
+      subcat_price: price
+    })
+  }
+
+  deleteItem(item) {
+    firebase.database().ref('users/' + this.currentUser + '/cart/' + item.uid + '/').remove();
   }
 
   presentActionSheet() {
@@ -36,13 +89,13 @@ export class CartPage {
             console.log('Destructive clicked');
           }
         },
-        {
-          text: 'Online Payment',
-          handler: () => {
-            this.payMethod = 'Online Payment'
-            console.log('Archive clicked');
-          }
-        },
+        // {
+        //   text: 'Online Payment',
+        //   handler: () => {
+        //     this.payMethod = 'Online Payment'
+        //     console.log('Archive clicked');
+        //   }
+        // },
         {
           text: 'Cancel',
           role: 'cancel',
--- src/pages/product-list/product-list.ts
+++ src/pages/product-list/product-list.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import * as firebase from 'firebase';
 
 @IonicPage()
 @Component({
@@ -27,6 +28,9 @@ export class ProductListPage {
     let subCatDetails = this.allCatDetails.subcategories;
     for(let key in subCatDetails) {
       subCatDetails[key].puid = key;
+      subCatDetails[key].image = this.allCatDetails.cat_image;
+      subCatDetails[key].qty = 1;
+      subCatDetails[key].original_price = subCatDetails[key].subcat_price;
       this.allItems.push(subCatDetails[key]);
     }
   }
@@ -36,6 +40,7 @@ export class ProductListPage {
   }
 
   addToCart(item, index){
+    firebase.database().ref('users/' +firebase.auth().currentUser.uid+'/cart/').push(item)
     alert('Successfully Added')
   }
   
